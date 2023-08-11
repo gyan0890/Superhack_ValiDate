9d619cde09950a55d8e30ae1452cffd180a6f418
gyan0890 <gyanlakshmi@gmail.com>
2023-08-12 01:51:28 +0530

    Changed path in index.js

--- validate-backend/zk-proof/index.js
+++ validate-backend/zk-proof/index.js
@@ -2,13 +2,13 @@ const snarkjs = require("snarkjs");
 const fs = require("fs");
 
 async function run() {
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve({"age": 32, "ageLimit": 32}, "./circuits/ageCheck_js/ageCheck.wasm", "../ageCheck_0001.zkey");
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve({"age": 32, "ageLimit": 32}, "./circuits/ageCheck_js/ageCheck.wasm", "ageCheck_0001.zkey");
 
     console.log("Proof: ");
     console.log(JSON.stringify(proof, null, 1));
     console.log("Public signals:");
     console.log(publicSignals);
-    const vKey = JSON.parse(fs.readFileSync("../verification_key.json"));
+    const vKey = JSON.parse(fs.readFileSync("verification_key.json"));
     console.log("Verification Key:");
     console.log(vKey);
     const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
