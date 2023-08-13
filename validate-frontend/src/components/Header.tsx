7bf3b27dec483013f18bf8e9f88f65e534aff95f
prathibhabilagi <prathibhabilagi12@gmail.com>
2023-08-13 13:34:26 +0530

    removed z-index

--- validate-frontend/src/components/Header.tsx
+++ validate-frontend/src/components/Header.tsx
@@ -17,20 +17,20 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="absolute inset-x-0 top-0 max-w-7xl mx-auto z-50">
+    <header className="absolute inset-x-0 top-0 max-w-7xl mx-auto">
       <nav
         className="flex items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
               className="h-8 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
             />
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
